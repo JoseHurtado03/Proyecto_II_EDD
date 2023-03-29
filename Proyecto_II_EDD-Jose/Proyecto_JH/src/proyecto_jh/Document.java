f7491912155af012bf3d5f8bd57a6758216b72bf
mojeda20 <moises.liota@correo.unimet.edu.ve>
2023-03-29 15:25:14 -0400

    :sparkles: Listo lectura del txt

---
+++ Proyecto_II_EDD-Jose/Proyecto_JH/src/proyecto_jh/Document.java
@@ -0,0 +1,72 @@
+
+package proyecto_jh;
+
+public class Document {
+    private String title;
+    private String authors;
+    private String summaryBody;
+    private String keyWords;
+
+    public Document(String title, String authors, String summaryBody, String keyWords) {
+        this.title = title;
+        this.authors = authors;
+        this.summaryBody = summaryBody;
+        this.keyWords = keyWords;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the authors
+     */
+    public String getAuthors() {
+        return authors;
+    }
+
+    /**
+     * @param authors the authors to set
+     */
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    /**
+     * @return the summaryBody
+     */
+    public String getSummaryBody() {
+        return summaryBody;
+    }
+
+    /**
+     * @param summaryBody the summaryBody to set
+     */
+    public void setSummaryBody(String summaryBody) {
+        this.summaryBody = summaryBody;
+    }
+
+    /**
+     * @return the keyWords
+     */
+    public String getKeyWords() {
+        return keyWords;
+    }
+
+    /**
+     * @param keyWords the keyWords to set
+     */
+    public void setKeyWords(String keyWords) {
+        this.keyWords = keyWords;
+    }
+}
---
+++ Proyecto_II_EDD-Jose/Proyecto_JH/src/proyecto_jh/Main.java
@@ -0,0 +1,11 @@
+
+package proyecto_jh;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cargar_Datos v1 = new Cargar_Datos();
+        v1.setVisible(true);
+    }
+}
