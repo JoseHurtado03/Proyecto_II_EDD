cc51fad6c0e3787728cd37d358358509d7f5aa77
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2023-03-29 20:50:49 -0400

    :white_check_mark: LISTO 100%

---
+++ PROYECTO_Carmona_Hurtado_Liota/src/proyecto_jh/Main.java
@@ -0,0 +1,12 @@
+
+package proyecto_jh;
+
+import Windows.wMain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        wMain window = new wMain();
+        window.setVisible(true);
+    }
+}
